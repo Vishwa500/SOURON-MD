6675fc6bbe9dd0ccaf854aefa6fbffe38a316e49
Vishwa500 <143321672+Vishwa500@users.noreply.github.com>
2024-12-17 19:19:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,21 +1,5 @@
-/*╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺
-    ⭐ＰＲＯＪＥＣＴ ＮＡＭＥ:
-    ＳＵＢＺＥＲＯ ＷＨＡＴＳＡＰＰ ＭＤ ＢＯＴ
-    
-    ⭐ＤＥＶＥＬＯＰＥＲ
-     ＭＲ ＦＲＡＮＫ 
-     
-    ⭐ ＭＹ ＴＥＡＭ
-     ＸＥＲＯ ＣＯＤＥＲＳ
-     
-    ⭐ ＯＵＲ ＷＥＢＳＩＴＥ
-     https://github.com/ZwSyntax/SUBZERO-MD
 
-© ＴＲＹ ＤＥＣＲＹＰＴＩＮＧ ＩＦ ＹＯＵ ＣＡＮ⚠
-
-╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺*/
-
-
+ 
 const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
@@ -23,7 +7,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 
-//--------------------------Mr Frank---------------------------------------
+//--------------------------VISHWA---------------------------------------
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "ENTER YOUR SESSION HERE",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
@@ -31,8 +15,8 @@ MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://i.postimg.cc/9fVpSkn7/IMG-20241213-WA0012.jpg",
-ALIVE_MSG: process.env.ALIVE_MSG || "sᴜʙᴢᴇʀᴏ ᴀɪ ɪs ᴀʟɪᴠᴇ. ᴋᴇᴇᴘ sᴜᴘᴘᴏʀᴛɪɴɢ *Zᴇʀᴏ Cᴏᴅᴇʀs❄️*\n\n> ᴅᴇᴠᴇʟᴏᴘᴇᴅ ʙʏ ᴍʀ ꜰʀᴀɴᴋ",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/WF67wzY/738.jpg",
+ALIVE_MSG: process.env.ALIVE_MSG || "ꜱᴏᴜʀᴏɴ-ᴍᴅ ᴡᴀ ᴀʟɪᴠᴇ ɴᴏᴡ☠\n\n> ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴠɪꜱʜᴡᴀ ᴀɴᴅ ꜱᴀᴅᴇᴇᴘᴀ",
 ANTI_LINK: process.env.ANTI_LINK || "true",
 ANTI_BAD: process.env.ANTI_BAD || "true",
 PREFIX: process.env.PREFIX || ".",
@@ -40,8 +24,8 @@ FAKE_RECORDING: process.env.FAKE_RECORDING || "false",
 AUTO_REACT: process.env.AUTO_REACT || "false",
 HEART_REACT: process.env.HEART_REACT || "false",
 OWNER_REACT: process.env.OWNER_REACT || "false",
-BOT_NAME: process.env.BOT_NAME || "『 ＳＵＢＺＥＲＯ - ＭＤ 』",
+BOT_NAME: process.env.BOT_NAME || "SOURON-MD",
 OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
 };
 
-//--------------MADE BY MR FRANK 2024 DEC---------------->-
+//--------------MADE by vishwa ---------------->-
